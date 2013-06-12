6adf8a053035ed221583543dc6affeec962be001
Léon van de Broek <leonvandebroek@me.com>
2013-06-12 14:33:39 +0200

    structuur

---
+++ BussinesLayer/Mindmap/Knoop.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BussinesLayer
+{
+    public class MindMap
+    {
+        public Knoop Knoop
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+
+    public class Knoop
+    {
+        public Opmaak Opmaak
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public Inhoud Inhoud
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
---
+++ BussinesLayer/Presentatie/Dia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BussinesLayer
+{
+    public class Dia
+    {
+        public Knoop Knoop
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public Notitie Notitie
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
