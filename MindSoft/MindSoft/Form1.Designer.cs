d37eaf827a544e9379e4cf767aca91c3b74c5298
VV3sl3y <wesleytummers@hotmail.com>
2013-06-13 19:02:33 +0200

    Form1 wat aangepast

--- MindSoft/MindSoft/Form1.Designer.cs
+++ MindSoft/MindSoft/Form1.Designer.cs
@@ -30,19 +30,28 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.newKnoopBtn = new System.Windows.Forms.Button();
+            this.delKnoopBtn = new System.Windows.Forms.Button();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAssToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomTBar = new System.Windows.Forms.TrackBar();
+            this.zoomCB = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zoomTBar)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
-            this.newToolStripMenuItem,
             this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
@@ -52,18 +61,20 @@
             // 
             // fileToolStripMenuItem
             // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem1,
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAssToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
             this.fileToolStripMenuItem.Text = "file";
             // 
-            // newToolStripMenuItem
-            // 
-            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
-            this.newToolStripMenuItem.Text = "new";
-            // 
             // aboutToolStripMenuItem
             // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem1});
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
             this.aboutToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
             this.aboutToolStripMenuItem.Text = "about";
@@ -74,27 +85,113 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(13, 52);
+            this.pictureBox1.Location = new System.Drawing.Point(13, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(521, 258);
+            this.pictureBox1.Size = new System.Drawing.Size(388, 283);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
+            // newKnoopBtn
             // 
-            this.button1.Location = new System.Drawing.Point(23, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(471, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.newKnoopBtn.Location = new System.Drawing.Point(407, 27);
+            this.newKnoopBtn.Name = "newKnoopBtn";
+            this.newKnoopBtn.Size = new System.Drawing.Size(127, 23);
+            this.newKnoopBtn.TabIndex = 2;
+            this.newKnoopBtn.Text = "Nieuwe Knoop";
+            this.newKnoopBtn.UseVisualStyleBackColor = true;
+            this.newKnoopBtn.Click += new System.EventHandler(this.newKnoopBtn_Click);
+            // 
+            // delKnoopBtn
+            // 
+            this.delKnoopBtn.Location = new System.Drawing.Point(407, 56);
+            this.delKnoopBtn.Name = "delKnoopBtn";
+            this.delKnoopBtn.Size = new System.Drawing.Size(127, 23);
+            this.delKnoopBtn.TabIndex = 3;
+            this.delKnoopBtn.Text = "Verwijder Knoop";
+            this.delKnoopBtn.UseVisualStyleBackColor = true;
+            this.delKnoopBtn.Click += new System.EventHandler(this.delKnoopBtn_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
+            // 
+            // newToolStripMenuItem1
+            // 
+            this.newToolStripMenuItem1.Name = "newToolStripMenuItem1";
+            this.newToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem1.Text = "New";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAssToolStripMenuItem
+            // 
+            this.saveAssToolStripMenuItem.Name = "saveAssToolStripMenuItem";
+            this.saveAssToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAssToolStripMenuItem.Text = "Save Ass";
+            // 
+            // zoomTBar
+            // 
+            this.zoomTBar.Location = new System.Drawing.Point(407, 294);
+            this.zoomTBar.Maximum = 250;
+            this.zoomTBar.Name = "zoomTBar";
+            this.zoomTBar.Size = new System.Drawing.Size(139, 45);
+            this.zoomTBar.TabIndex = 4;
+            // 
+            // zoomCB
+            // 
+            this.zoomCB.FormattingEnabled = true;
+            this.zoomCB.Items.AddRange(new object[] {
+            "25%",
+            "50%",
+            "75%",
+            "100%",
+            "150%",
+            "200%",
+            "250%"});
+            this.zoomCB.Location = new System.Drawing.Point(479, 276);
+            this.zoomCB.Name = "zoomCB";
+            this.zoomCB.Size = new System.Drawing.Size(67, 21);
+            this.zoomCB.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(436, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Zoom:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(546, 322);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zoomCB);
+            this.Controls.Add(this.zoomTBar);
+            this.Controls.Add(this.delKnoopBtn);
+            this.Controls.Add(this.newKnoopBtn);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -104,6 +201,7 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zoomTBar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,10 +211,19 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button newKnoopBtn;
+        private System.Windows.Forms.Button delKnoopBtn;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAssToolStripMenuItem;
+        private System.Windows.Forms.TrackBar zoomTBar;
+        private System.Windows.Forms.ComboBox zoomCB;
+        private System.Windows.Forms.Label label1;
 
     }
 }
