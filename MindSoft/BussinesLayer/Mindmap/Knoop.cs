37c3e394defdd3203f959e43a648ae3c47d4ec83
rhiensch <info@robhiensch.nl>
2013-06-16 23:03:34 +0200

    knopen

--- MindSoft/BussinesLayer/Mindmap/Knoop.cs
+++ MindSoft/BussinesLayer/Mindmap/Knoop.cs
@@ -2,25 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace BussinesLayer
 {
 
     public class Knoop
     {
+        #region attributen
+        private Opmaak huidigeOpmaak;
+
+
         public Opmaak Opmaak
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
+            get { return huidigeOpmaak; }
+            set { huidigeOpmaak = value; }
         }
 
         public IList<Inhoud> inhoudlist;
+        public Point positie { get; set; }
 
-        public System.Drawing.Point positie { get; set; }
+        #endregion
     }
 }
